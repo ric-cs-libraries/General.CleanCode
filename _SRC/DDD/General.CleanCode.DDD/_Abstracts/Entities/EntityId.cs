b3d36f283c0623ed102025674a67d44f4b1dc9f9
Richard PAYET <rick.info.rsoc@gmail.com>
2024-09-25 12:00:27 +0200

    General.CleanCode.DDD: EntityId<T> : ajout  contrainte "struct" sur T.

--- _SRC/DDD/General.CleanCode.DDD/_Abstracts/Entities/EntityId.cs
+++ _SRC/DDD/General.CleanCode.DDD/_Abstracts/Entities/EntityId.cs
@@ -1,3 +1,5 @@
 ﻿namespace General.CleanCode.DDD;
 
-public abstract record EntityId<TId>(TId Value) : ValueObject;
+public abstract record EntityId<TId>(TId Value) : ValueObject
+    where TId : struct //Type valeur non nullabe
+;
