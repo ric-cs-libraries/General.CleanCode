dace3c087edf2b621114130ceb324c231db0aa3c
Richard PAYET <rick.info.rsoc@gmail.com>
2025-01-13 15:18:33 +0100

    General.CleanCode.DDD v4.0: ajout membre CreationDateUtc à IDomainEvent, et ajout abstract class: DomainEvent. No test.

---
+++ _SRC/DDD/General.CleanCode.DDD/_Abstracts/Events/DomainEvent.cs
@@ -0,0 +1,9 @@
+﻿using General.CleanCode.DDD.Interfaces;
+
+
+namespace General.CleanCode.DDD.Abstracts.Events;
+
+public abstract record DomainEvent : IDomainEvent
+{
+    public DateTime CreationDateUtc { get; } = DateTime.UtcNow;
+}
